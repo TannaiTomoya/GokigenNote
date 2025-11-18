e6b42172a67a08c2d099235de5c47b2743a73d64
Tomoya <tomobou5912@gmail.com>
2025-11-19 02:46:37 +0900

    Initial Commit

---
+++ GokigenNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GokigenNote
+//
+//  Created by 丹内智弥 on 2025/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GokigenNote/GokigenNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GokigenNoteApp.swift
+//  GokigenNote
+//
+//  Created by 丹内智弥 on 2025/11/19.
+//
+
+import SwiftUI
+
+@main
+struct GokigenNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
